2745b732bf96010d7be08083f6fd777e67a3498c
Mark Sayson <masayson@gmail.com>
2016-04-22 16:44:50 -0700

    Added simple styling for text and links

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,6 +2,6 @@ module ProductsHelper
    def sortableColumn(column, title = nil)
       title ||= column.titleize #Titlecase name if exists
       direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-      link_to title, {:sort => column, :direction => direction}
+      link_to title, {:sort => column, :direction => direction}, {:class => 'sortableColumn'}
    end
 end
