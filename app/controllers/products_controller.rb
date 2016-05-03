2415ef09af864926201d68d2c74c86af56ebf78f
Mark Sayson <masayson@gmail.com>
2016-05-03 16:52:00 -0700

    Addressed rubocop warnings

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,46 +1,48 @@
+# Controller for Product dashboard
 class ProductsController < ApplicationController
-   #Expose sort_column and sort_direction methods to helper functions
-   helper_method :sort_column, :sort_direction
+  # Expose sort_column and sort_direction methods to helper functions
+  helper_method :sort_column, :sort_direction
 
-   def index
-      @productsbytitle = Product.order(product_order_params)
-      @statuscounts = Product.status_counts
-   end
+  def index
+    @productsbytitle = Product.order(product_order_params)
+    @statuscounts = Product.status_counts
+  end
 
-   def show
-      @product = Product.find(params[:id])
-   end
+  def show
+    @product = Product.find(params[:id])
+  end
 
-   def new
-      @product = Product.new
-   end
+  def new
+    @product = Product.new
+  end
 
-   def create
-      @product = Product.new(product_params)
+  def create
+    @product = Product.new(product_params)
 
-      if @product.save
-         redirect_to @product
-      else
-         render 'new'
-      end
-   end
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
 
-   private
-      def product_params
-         params.require(:product).permit(:title, :status, :supportedFeatures)
-      end
+  private
 
-      def product_order_params
-         sort_column + " " + sort_direction
-      end
+  def product_params
+    params.require(:product).permit(:title, :status, :supportedFeatures)
+  end
 
-      def sort_column
-         #Default to sorting by title
-         Product.column_names.include?(params[:sort]) ? params[:sort] : "title"
-      end
+  def product_order_params
+    sort_column + ' ' + sort_direction
+  end
 
-      def sort_direction
-         #Default to ascending order
-         %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-      end
+  def sort_column
+    # Default to sorting by title
+    Product.column_names.include?(params[:sort]) ? params[:sort] : 'title'
+  end
+
+  def sort_direction
+    # Default to ascending order
+    %w(asc desc).include?(params[:direction]) ? params[:direction] : 'asc'
+  end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,4 @@
+# Controller for welcome page
 class WelcomeController < ApplicationController
   def index
   end
