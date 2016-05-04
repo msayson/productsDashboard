519a27de1f8cc7502ce5c37e6f8d12ca2cfc7fec
Mark Sayson <masayson@gmail.com>
2016-05-03 20:56:12 -0700

    Addressed rubocop error in test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
-#Loads test data from fixtures/products.yml
+# Note: Loads test data from fixtures/products.yml
 class ProductTest < ActiveSupport::TestCase
-  test "status_counts.count" do
+  test 'status_counts.count' do
     assert_equal 3, Product.status_counts.count
   end
 
-  test "status_counts contents" do
+  test 'status_counts contents' do
     assert_equal 3, Product.status_counts['Deployed']
     assert_equal 1, Product.status_counts['Planned']
     assert_equal 1, Product.status_counts['Released']
