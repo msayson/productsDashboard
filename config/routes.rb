5c6958867b5fb99fced92772a65ea2806fca2db2
Mark Sayson <masayson@gmail.com>
2016-05-03 21:10:41 -0700

    Removed obsolete resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   resources :products
 
-  resources :productSummaries, controller: 'product_summaries'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
