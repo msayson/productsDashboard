c2b6c1272a9655605b5259f5b11f5219e1cb3c69
Mark Sayson <masayson@gmail.com>
2016-05-12 20:56:53 -0700

    Test saving product models

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,6 +2,21 @@ require 'test_helper'
 
 # Note: Loads test data from fixtures/products.yml
 class ProductTest < ActiveSupport::TestCase
+  test 'should not save without title' do
+    product = Product.new(status: 'Planned')
+    assert_not product.save
+  end
+
+  test 'should not save without status' do
+    product = Product.new(title: 'Product 1')
+    assert_not product.save
+  end
+
+  test 'can save when required fields are present' do
+    product = Product.new(title: 'Product 1', status: 'Planned')
+    assert product.save
+  end
+
   test 'status_counts.count' do
     assert_equal 3, Product.status_counts.count
   end
