54b23952b829b0e69f6244880857edd2f8fd5247
Mark Sayson <masayson@gmail.com>
2016-05-12 21:35:37 -0700

    Added initial tests for ProductsController actions

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
+# Tests for ProductController actions and other methods
 class ProductsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Test can get all pages
+  test 'should get index' do
+    get :index
+    assert_response :success
+  end
+
+  test 'should get show if product exists' do
+    Product.create(id: 1, title: 'Product1', status: 'JIRA')
+    get :show, id: 1
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+  end
+
+  # Test create action renders/redirects to correct pages
+  test 'should render new on invalid create' do
+    get :create, product: { status: 'Planned' }
+    assert !response.error?, 'Should handle errors instead of passing them'
+    assert_template :new
+  end
+
+  test 'should generate index' do
+    assert_generates '/products', controller: 'products', action: 'index'
+  end
+
+  test 'should generate show' do
+    assert_generates '/products/4', controller: 'products', action: 'show',
+                                    id: 4
+  end
+
+  test 'should generate new' do
+    assert_generates '/products/new', controller: 'products', action: 'new'
+  end
 end
